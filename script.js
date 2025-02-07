7585bcbc9b4e4a3aed5ea63dd41f0bea20f91d9e
abhaysubramanian <abhaysubramanian209@gmail.com>
2025-02-08 00:16:33 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    emailjs.init("gfAkgIhxc_2S7ZzB5"); // Replace with your EmailJS User ID
-
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        let name = document.getElementById('name').value;
-        let email = document.getElementById('email').value;
-        let message = document.getElementById('message').value;
-
-        if (!name || !email || !message) {
-            alert('Please fill out all fields.');
-            return;
-        }
-
-        emailjs.send("service_q388qxm", "template_2zz523l", {
-            name: name,
-            email: email,
-            subject:message,
-            message: message,
-           
-        })
-        .then(() => {
-            alert(`Thank you, ${name}! Your message has been sent.`);
-            document.getElementById('contact-form').reset();
-        })
-        .catch((error) => {
-            alert('Failed to send message. Please try again.');
-            console.error("EmailJS Error:", error);
-        });
-    });
-
+    
     document.getElementById("download-btn").addEventListener("click", function () {
         const link = document.createElement("a");
-        link.href = "./images/abhaypdf.pdf"; // Ensure the correct file path
-        link.download = "Abhay_Subramanian_Resume.pdf"; // Suggested filename
+        link.href = "./images/abhaypdf.pdf"; 
+        link.download = "Abhay_Subramanian_Resume.pdf"; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
